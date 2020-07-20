f6125b1dc79f9b9cd3ee7672ad8cb679f101025f
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-07-21 00:07:46 +0300

    Login Functionality | Firebase Authentication

--- app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
+++ app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
@@ -15,6 +15,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
 import com.stathis.moviepedia.models.User
+import kotlinx.android.synthetic.main.activity_intro_screen.*
 import kotlinx.android.synthetic.main.login_view.view.*
 import kotlinx.android.synthetic.main.register_view.*
 import kotlinx.android.synthetic.main.register_view.view.*
@@ -23,9 +24,6 @@ import kotlinx.android.synthetic.main.register_view.view.registerEmailField
 class IntroScreen : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
-    private lateinit var loginBtn: Button
-    private lateinit var registerBtn: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,12 +40,11 @@ class IntroScreen : AppCompatActivity() {
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        loginBtn = findViewById(R.id.login_btn)
-        registerBtn = findViewById(R.id.registerBtn)
 
-        loginBtn.setOnClickListener {
+        login_btn.setOnClickListener {
             showLoginDialogue()
         }
+
         registerBtn.setOnClickListener {
             showRegisterDialogue()
         }
@@ -55,18 +52,54 @@ class IntroScreen : AppCompatActivity() {
 
     private fun showLoginDialogue() {
         //Inflate the dialog with custom view
-        val LoginDialogView = LayoutInflater.from(this).inflate(R.layout.login_view, null)
+        val loginDialog = LayoutInflater.from(this).inflate(R.layout.login_view, null)
         //AlertDialogBuilder
         val loginBuilder = AlertDialog.Builder(this)
-            .setView(LoginDialogView)
+            .setView(loginDialog)
         //show dialog
-        val mAlertDialog = loginBuilder.show()
+        loginBuilder.show()
         //login button click of custom layout
-        LoginDialogView.loginAccBtn.setOnClickListener {
-            //dismiss dialog
-            mAlertDialog.dismiss()
-            val register = Intent(this, SplashScreen::class.java)
-            startActivity(register)
+        loginDialog.loginAccBtn.setOnClickListener {
+            val emailField: TextInputEditText = loginDialog.findViewById(R.id.loginEmailField)
+            val passwordField: TextInputEditText = loginDialog.findViewById(R.id.loginPasswordField)
+
+            if (emailField.text.toString().isEmpty()) {
+                emailField.error = "Please enter your e-mail"
+                emailField.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailField.text.toString()).matches()) {
+                emailField.error = "Please enter a valid e-mail address"
+                emailField.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (passwordField.text.toString().isEmpty()) {
+                passwordField.error = "Please enter your password"
+                passwordField.requestFocus()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(
+                emailField.text.toString(),
+                passwordField.text.toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        updateUI(user)
+                        startActivity(Intent(this, Dashboard::class.java))
+                        finish() //kills IntroScreenApp
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Login Failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        updateUI(null)
+                    }
+                }
         }
     }
 
@@ -82,7 +115,7 @@ class IntroScreen : AppCompatActivity() {
             val emailField: TextInputEditText = registerDialog.findViewById(R.id.registerEmailField)
             val passwordField: TextInputEditText =
                 registerDialog.findViewById(R.id.registerPasswordField)
-            val passwordConfigfield: TextInputEditText =
+            val passwordConfigField: TextInputEditText =
                 registerDialog.findViewById(R.id.registerPasswordConfirmField)
 
             if (emailField.text.toString().isEmpty()) {
@@ -103,22 +136,20 @@ class IntroScreen : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            if (passwordConfigfield.text.toString().isEmpty()) {
-                passwordConfigfield.error = "Please confirm your password"
-                passwordConfigfield.requestFocus()
+            if (passwordConfigField.text.toString().isEmpty()) {
+                passwordConfigField.error = "Please confirm your password"
+                passwordConfigField.requestFocus()
                 return@setOnClickListener
             }
 
-            if (passwordField.text.toString() != passwordConfigfield.text.toString()) {
+            if (passwordField.text.toString() == passwordConfigField.text.toString()) {
                 auth.createUserWithEmailAndPassword(
                     emailField.text.toString(),
                     passwordField.text.toString()
                 )
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            val goToHomePage = Intent(this, SplashScreen::class.java)
-                            startActivity(goToHomePage)
-                            finish() //kills the intro Screen
+                            registerDialog.dismiss()
                         }
                     }
             }
@@ -126,7 +157,5 @@ class IntroScreen : AppCompatActivity() {
     }
 
     private fun updateUI(user: FirebaseUser?) {
-
     }
 }
-
