f7e190fe25b7ee41e5ebf1c2a0a93676f871bd81
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-07-20 14:56:49 +0300

    Login & Register Screens

--- app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
+++ app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
@@ -1,14 +1,24 @@
 package com.stathis.moviepedia
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.login_view.*
+import kotlinx.android.synthetic.main.login_view.view.*
+import kotlinx.android.synthetic.main.register_view.view.*
 
 class IntroScreen : AppCompatActivity() {
 
-    private lateinit var loginBtn:Button
-    private lateinit var registerBtn:Button
+    private lateinit var loginBtn: Button
+    private lateinit var registerBtn: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +27,46 @@ class IntroScreen : AppCompatActivity() {
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
+        loginBtn = findViewById(R.id.login_btn)
+        registerBtn = findViewById(R.id.registerBtn)
 
-        loginBtn.setOnClickListener{
-            val login = Intent(this, SplashScreen::class.java)
-            startActivity(login)
+        loginBtn.setOnClickListener {
+            showLoginDialogue()
         }
-        registerBtn.setOnClickListener{
+        registerBtn.setOnClickListener {
+            showRegisterDialogue()
+        }
+    }
+
+    private fun showLoginDialogue() {
+        //Inflate the dialog with custom view
+        val LoginDialogView = LayoutInflater.from(this).inflate(R.layout.login_view, null)
+        //AlertDialogBuilder
+        val loginBuilder = AlertDialog.Builder(this)
+            .setView(LoginDialogView)
+        //show dialog
+        val mAlertDialog = loginBuilder.show()
+        //login button click of custom layout
+        LoginDialogView.loginAccBtn.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+            val register = Intent(this, SplashScreen::class.java)
+            startActivity(register)
+        }
+    }
+
+    private fun showRegisterDialogue() {
+        //Inflate the dialog with custom view
+        val RegisterDialogView = LayoutInflater.from(this).inflate(R.layout.register_view, null)
+        //AlertDialogBuilder
+        val registerBuilder = AlertDialog.Builder(this)
+            .setView(RegisterDialogView)
+        //show dialog
+        val mAlertDialog = registerBuilder.show()
+        //login button click of custom layout
+        RegisterDialogView.registerAccBtn.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
             val register = Intent(this, SplashScreen::class.java)
             startActivity(register)
         }
--- app/src/main/java/com/stathis/moviepedia/SplashScreen.kt
+++ app/src/main/java/com/stathis/moviepedia/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.stathis.moviepedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this, IntroScreen::class.java))
+            //finish this activity
+            finish()
+        },4000)
     }
 }
