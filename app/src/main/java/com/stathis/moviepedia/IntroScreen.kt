7ab37afd7d423265f3f924bd1f57297978e1d63b
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-07-20 23:36:05 +0300

    User Register | Firebase Authentication

--- app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
+++ app/src/main/java/com/stathis/moviepedia/IntroScreen.kt
@@ -4,18 +4,25 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.android.synthetic.main.login_view.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.stathis.moviepedia.models.User
 import kotlinx.android.synthetic.main.login_view.view.*
+import kotlinx.android.synthetic.main.register_view.*
 import kotlinx.android.synthetic.main.register_view.view.*
+import kotlinx.android.synthetic.main.register_view.view.registerEmailField
 
 class IntroScreen : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var loginBtn: Button
     private lateinit var registerBtn: TextView
@@ -23,6 +30,14 @@ class IntroScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_screen)
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
@@ -57,18 +72,61 @@ class IntroScreen : AppCompatActivity() {
 
     private fun showRegisterDialogue() {
         //Inflate the dialog with custom view
-        val RegisterDialogView = LayoutInflater.from(this).inflate(R.layout.register_view, null)
+        val registerDialogView = LayoutInflater.from(this).inflate(R.layout.register_view, null)
         //AlertDialogBuilder
         val registerBuilder = AlertDialog.Builder(this)
-            .setView(RegisterDialogView)
+            .setView(registerDialogView)
         //show dialog
-        val mAlertDialog = registerBuilder.show()
-        //login button click of custom layout
-        RegisterDialogView.registerAccBtn.setOnClickListener {
-            //dismiss dialog
-            mAlertDialog.dismiss()
-            val register = Intent(this, SplashScreen::class.java)
-            startActivity(register)
+        val registerDialog = registerBuilder.show()
+        registerDialogView.registerAccBtn.setOnClickListener {
+            val emailField: TextInputEditText = registerDialog.findViewById(R.id.registerEmailField)
+            val passwordField: TextInputEditText =
+                registerDialog.findViewById(R.id.registerPasswordField)
+            val passwordConfigfield: TextInputEditText =
+                registerDialog.findViewById(R.id.registerPasswordConfirmField)
+
+            if (emailField.text.toString().isEmpty()) {
+                emailField.error = "Please enter your e-mail"
+                emailField.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailField.text.toString()).matches()) {
+                emailField.error = "Please enter a valid e-mail address"
+                emailField.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (passwordField.text.toString().isEmpty()) {
+                passwordField.error = "Please enter your password"
+                passwordField.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (passwordConfigfield.text.toString().isEmpty()) {
+                passwordConfigfield.error = "Please confirm your password"
+                passwordConfigfield.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (passwordField.text.toString() != passwordConfigfield.text.toString()) {
+                auth.createUserWithEmailAndPassword(
+                    emailField.text.toString(),
+                    passwordField.text.toString()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val goToHomePage = Intent(this, SplashScreen::class.java)
+                            startActivity(goToHomePage)
+                            finish() //kills the intro Screen
+                        }
+                    }
+            }
         }
     }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
 }
+
---
+++ app/src/main/java/com/stathis/moviepedia/models/User.kt
@@ -0,0 +1,8 @@
+package com.stathis.moviepedia.models
+
+data class User(
+    val id: String?,
+    var email:String,
+    var password:String
+)
+
