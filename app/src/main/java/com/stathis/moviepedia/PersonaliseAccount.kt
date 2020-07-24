686e3d35f3c95593aa2984d22a3473a91bab11af
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-07-24 17:26:08 +0300

    Dashboard

--- app/src/main/java/com/stathis/moviepedia/PersonaliseAccount.kt
+++ app/src/main/java/com/stathis/moviepedia/PersonaliseAccount.kt
@@ -1,12 +1,22 @@
 package com.stathis.moviepedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_personalise_account.*
 
 class PersonaliseAccount : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personalise_account)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        skipBtn.setOnClickListener{
+            startActivity(Intent(this, Dashboard::class.java))
+        }
     }
 }
