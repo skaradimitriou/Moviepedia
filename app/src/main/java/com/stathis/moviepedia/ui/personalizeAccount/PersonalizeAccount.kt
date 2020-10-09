9f98ef33b5d460f3b25e958736b4a11a37ac2c19
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2020-10-09 14:04:40 +0300

    General Refactoring before release

--- app/src/main/java/com/stathis/moviepedia/ui/personalizeAccount/PersonalizeAccount.kt
+++ app/src/main/java/com/stathis/moviepedia/ui/personalizeAccount/PersonalizeAccount.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -14,14 +15,15 @@ import com.stathis.moviepedia.R
 import com.stathis.moviepedia.databinding.ActivityPersonalizeAccountBinding
 import com.stathis.moviepedia.ui.dashboard.Dashboard
 import kotlinx.android.synthetic.main.bottom_sheet_choose_option.view.*
+import java.lang.Exception
 
 class PersonalizeAccount : AppCompatActivity() {
 
     private val REQUEST_IMAGE_CAPTURE = 100
     private val IMAGE_PICK_CODE = 200;
     private val PERMISSION_CODE = 201;
-    private lateinit var binding:ActivityPersonalizeAccountBinding
-    private lateinit var viewModel : PersonalizeAccountViewModel
+    private lateinit var binding: ActivityPersonalizeAccountBinding
+    private lateinit var viewModel: PersonalizeAccountViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,12 +36,12 @@ class PersonalizeAccount : AppCompatActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        binding.personalisePhoto.setOnClickListener{
+        binding.personalisePhoto.setOnClickListener {
             showUploadPhotoOptions()
         }
 
-        binding.getStartedBtn.setOnClickListener{
-            val username:String = binding.username.text.toString()
+        binding.getStartedBtn.setOnClickListener {
+            val username: String = binding.username.text.toString()
             viewModel.saveUsername(username)
             startActivity(Intent(this, Dashboard::class.java))
         }
@@ -58,16 +60,20 @@ class PersonalizeAccount : AppCompatActivity() {
         }
     }
 
-    private fun takePictureIntent(){
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{ pictureIntent ->
-            pictureIntent.resolveActivity(this.packageManager!!)?.also{
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { pictureIntent ->
+            pictureIntent.resolveActivity(this.packageManager!!)?.also {
                 startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
             }
         }
     }
 
     private fun uploadFromGallery() {
-        Intent(Intent.ACTION_PICK).also { uploadFromCamera ->
+//        val gallery = Intent(Intent.ACTION_PICK)
+//        gallery.putExtra("image/*")
+
+        Intent(Intent.ACTION_GET_CONTENT).also { uploadFromCamera ->
+            uploadFromCamera.type = "image/*"
             uploadFromCamera.resolveActivity(this.packageManager!!)?.also {
                 startActivityForResult(uploadFromCamera, IMAGE_PICK_CODE)
             }
@@ -81,8 +87,13 @@ class PersonalizeAccount : AppCompatActivity() {
             val imgBitmap = data?.extras?.get("data") as Bitmap
             viewModel.uploadAndSaveBitmapUri(imgBitmap)
         } else if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            val imgBitmap = data?.extras?.get("data") as Bitmap
-            viewModel.uploadAndSaveBitmapUri(imgBitmap)
+            try {
+//                val imgUri: Uri? = data?.data
+                val imgBitmap = data?.extras?.get("data") as Bitmap
+                viewModel.uploadAndSaveBitmapUri(imgBitmap)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
@@ -91,14 +102,14 @@ class PersonalizeAccount : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when(requestCode){
+        when (requestCode) {
             PERMISSION_CODE -> {
-                if (grantResults.size >0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     //permission from popup granted
                     uploadFromGallery()
-                }
-                else{
+                } else {
                     //permission from popup denied
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
