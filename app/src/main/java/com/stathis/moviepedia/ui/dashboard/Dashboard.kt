28cc1cb7a87847b98924123acc713adae2623450
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2021-02-28 10:55:53 +0200

    Navigation Component

--- app/src/main/java/com/stathis/moviepedia/ui/dashboard/Dashboard.kt
+++ app/src/main/java/com/stathis/moviepedia/ui/dashboard/Dashboard.kt
@@ -1,37 +1,27 @@
 package com.stathis.moviepedia.ui.dashboard
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stathis.moviepedia.R
 import com.stathis.moviepedia.databinding.ActivityDashboardBinding
-import com.stathis.moviepedia.ui.dashboard.fragments.all.DashboardFragment
-import com.stathis.moviepedia.ui.dashboard.fragments.movies.MoviesFragment
 import com.stathis.moviepedia.ui.dashboard.fragments.search.SearchFragment
-import com.stathis.moviepedia.ui.dashboard.fragments.tvSeries.TvSeriesFragment
 import com.stathis.moviepedia.ui.userProfile.UserProfile
 import com.stathis.moviepedia.ui.userProfile.UserViewModel
 
 class Dashboard : AppCompatActivity() {
 
     private lateinit var searchFragment: SearchFragment
-    private lateinit var dashboardFragment: DashboardFragment
-    private lateinit var moviesFragment: MoviesFragment
-    private lateinit var tvSeriesFragment: TvSeriesFragment
     private lateinit var binding: ActivityDashboardBinding
-    private var userViewModel: UserViewModel =
-        UserViewModel()
+    private var viewModel = UserViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +32,12 @@ class Dashboard : AppCompatActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        userViewModel.getUserPhoto().observe(this, Observer<String> { img ->
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        val navController = findNavController(R.id.fragment)
+
+        navView.setupWithNavController(navController)
+
+        viewModel.getUserPhoto().observe(this, Observer<String> { img ->
             Log.d("profile image path", img.toString())
             Glide.with(this).load(img).into(binding.userProfileImg)
         })
@@ -64,7 +59,7 @@ class Dashboard : AppCompatActivity() {
                 val bundle = bundleOf("QUERY" to query)
                 searchFragment =
                     SearchFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.content, searchFragment)
+                supportFragmentManager.beginTransaction().replace(R.id.fragment, searchFragment)
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .commit()
                 searchFragment.arguments = bundle
@@ -78,89 +73,9 @@ class Dashboard : AppCompatActivity() {
 
         })
 
-        if (isNetworkAvailable(this)) {
-            Log.d("Internet OK", "Internet OK")
-            dashboardFragment = DashboardFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.content, dashboardFragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .commit()
-        } else {
-            Log.d("No Internet", "No Internet")
-        }
-
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
-        bottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_home -> {
-                    dashboardFragment =
-                        DashboardFragment()
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.content, dashboardFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.nav_movies -> {
-                    moviesFragment =
-                        MoviesFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, moviesFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.nav_tv -> {
-                    tvSeriesFragment =
-                        TvSeriesFragment()
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.content, tvSeriesFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.nav_search -> {
-                    searchFragment =
-                        SearchFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, searchFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
-            }
-            bottomNav.selectedItemId = R.id.nav_home
-            false
-        })
     }
 
     override fun onBackPressed() {
         // You can't go back | Logout if you want to leave
-    }
-
-    private fun isNetworkAvailable(context: Context?): Boolean {
-        if (context == null) return false
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val capabilities =
-                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (capabilities != null) {
-                when {
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                        return true
-                    }
-                }
-            }
-        } else {
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-                return true
-            }
-        }
-        return false
     }
 }
