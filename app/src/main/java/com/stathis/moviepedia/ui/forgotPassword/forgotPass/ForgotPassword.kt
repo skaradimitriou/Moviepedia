a124e9a4772badcec81417baa51f156d38be7890
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2021-03-05 23:55:17 +0200

    ForgotPassword refactor

--- app/src/main/java/com/stathis/moviepedia/ui/forgotPassword/forgotPass/ForgotPassword.kt
+++ app/src/main/java/com/stathis/moviepedia/ui/forgotPassword/forgotPass/ForgotPassword.kt
@@ -6,73 +6,63 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.stathis.moviepedia.R
+import com.stathis.moviepedia.abstraction.AbstractActivity
 import com.stathis.moviepedia.databinding.ActivityForgotPasswordBinding
 import com.stathis.moviepedia.ui.loginAndRegister.IntroScreen
 import kotlinx.android.synthetic.main.account_success_view.view.*
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
-class ForgotPassword : AppCompatActivity() {
+class ForgotPassword : AbstractActivity() {
 
     private lateinit var binding: ActivityForgotPasswordBinding
     private lateinit var viewModel: ForgotPasswordViewModel
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
         binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
+    override fun initLayout() {
+        viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
+    }
 
-        auth = FirebaseAuth.getInstance()
-
+    override fun running() {
         send_activation_email.setOnClickListener {
-            verifyEmail()
+            viewModel.verifyEmail(forgot_email_field.text.toString())
         }
+
+        observeViewModel()
     }
 
-    private fun verifyEmail() {
-        auth.fetchSignInMethodsForEmail(forgot_email_field.text.toString())
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val isNewUser: Boolean? = it.result?.signInMethods?.isEmpty()
-                    if (isNewUser!!) {
-                        Log.e("TAG", "Is New User!");
-                    } else {
-                        sendEmail()
-                    }
-                }
+    private fun observeViewModel() {
+        viewModel.userVerified.observe(this, Observer {
+            when (it) {
+                true -> showSuccessDialog()
+
+                false -> Toast.makeText(this, "User not found", Toast.LENGTH_LONG).show()
             }
+        })
     }
 
-    private fun sendEmail() {
-        auth.sendPasswordResetEmail(forgot_email_field.text.toString())
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    showSuccessDialog()
-                }
-            }
+    override fun stopped() {
+        viewModel.userVerified.removeObservers(this)
     }
+
 
     private fun showSuccessDialog() {
-        //Inflate the dialog with custom view
         val successDialog = LayoutInflater.from(this).inflate(R.layout.account_success_view, null)
-        //AlertDialogBuilder
         val successBuilder = AlertDialog.Builder(this)
-            .setView(successDialog)
+            .setView(successDialog).show()
         successDialog.redirect_txt.text = "Follow the steps in your e-mail to reset your password"
-        //show dialog
-        val successDialogue = successBuilder.show()
-        //2 second delay
         Handler().postDelayed({
-            successDialogue.dismiss()
+            successBuilder.dismiss()
             startActivity(Intent(this, IntroScreen::class.java))
             finish()
         }, 4000)
--- app/src/main/java/com/stathis/moviepedia/ui/forgotPassword/forgotPass/ForgotPasswordViewModel.kt
+++ app/src/main/java/com/stathis/moviepedia/ui/forgotPassword/forgotPass/ForgotPasswordViewModel.kt
@@ -1,13 +1,37 @@
 package com.stathis.moviepedia.ui.forgotPassword.forgotPass
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordViewModel : ViewModel() {
 
-    private lateinit var auth:FirebaseAuth
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    val userVerified = MutableLiveData<Boolean>()
 
-    fun validateUser(){
+    fun verifyEmail(email : String) {
+        auth.fetchSignInMethodsForEmail(email)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val isNewUser: Boolean? = it.result?.signInMethods?.isEmpty()
+                    if (isNewUser!!) {
+                        Log.e("TAG", "Is New User!")
+                        userVerified.value = false
+                    } else {
+                        sendEmail(email)
+                    }
+                }
+            }
+    }
 
+    private fun sendEmail(email : String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    userVerified.value = true
+                }
+            }
     }
 }
